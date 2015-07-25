0093b0d8a878808c2b7e9eb70f0ee7ba10697e2e
Debra Turner <dturnermn@gmail.com>
2015-07-25 16:14:03 -0500

    Added input validation and comments to first makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -10,27 +10,54 @@
 ##	Objective: Create a special "matrix" object that can cache its inverse.
 ## 
 ## 	Input:  
-##		x	n x n Matrix
+##		x 	n x n Matrix
 ## 
 ##	Output:	
-##		"matrix" object
+##		list (set="function(y)",  
+##          get="function()", 
+##          setsolve="function(solve)",
+##          getsolve="function()"
+##         )
 ##
 makeCacheMatrix <- function(x = matrix()) {
 
   ## Validate input.  Exit function if input is not a matrix.
 	if (!is.matrix(x)) {
-		return("Invalid input. Expecting a matrix object.")
+		stop("Invalid input. Expecting a matrix object.")
 	}
   
+  ## Only square matrices can be inverted.  
+  ## Verify that the input matrix is a square matrix
+  
+  dm <- dim(x)
+  mrow <- dm[1]
+  mcol <- dm[2]
+  if (mrow != mcol) {
+    stop ("Invalid input.  Expecting a square matrix")
+  }
+  
+  ## Input must be a numeric matrix
+  if (!is.numeric(x[1,1])) {
+    stop("Invalid input.  Expecting a numeric matrix")
+  }
+  
+  ## Set the value of the matrix
   m <- NULL
   set <- function(y) {
             x <<- y
             m <<- NULL
   }
   
+  ## Get the value of the matrix
   get <- function() x
+  
+  ## Set the value of matrix inverse
   setsolve <- function(solve) m <<- solve
+  
+  ## Get the value of the matrix inverse
   getsolve <- function() m
+  
+  ## Return the special "matrix" object - a list of getters and setters
   list(set = set, get = get,
       setsolve = setsolve,
       getsolve = getsolve)
@@ -53,13 +80,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ##		Inverse of the matrix that was the input to function makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
+## Return a matrix that is the inverse of x
   m <- x$getsolve()
-  if(!is.null(m)) {
+  if (!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+  
   data <- x$get()
   m <- solve(data, ...)
   x$setsolve(m)
   m
+  
 }
+
